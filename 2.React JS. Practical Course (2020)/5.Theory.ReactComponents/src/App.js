072f49309e8fa492b05ce3dfa73b94e443771d86
zhenya-paitash <naitavv93@gmail.com>
2020-08-29 18:40:39 +0300

    5.Theory.ReactComponents

---
+++ 2.React JS. Practical Course (2020)/5.Theory.ReactComponents/src/App.js
@@ -0,0 +1,110 @@
+import React, { Component } from 'react'
+import './App.scss'
+import Car from './Car/Car'
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary'
+import Counter from './Counter/Counter'
+
+export const ClickedContext = React.createContext(false)
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      clicked: false,
+      cars: [
+        {name: 'Ford', year: 2018},
+        {name: 'Audi', year: 2016},
+        {name: 'Mazda', year: 2010}
+      ],
+      pageTitle: 'React components',
+      showCars: false
+    }
+  }
+
+  toggleCarsHandler = () => {
+    this.setState({
+      showCars: !this.state.showCars
+    })
+  }
+
+  onChangeName(name, index) {
+    const car = this.state.cars[index]
+    car.name = name
+    const cars = [...this.state.cars]
+    cars[index] = car
+    this.setState({cars})
+  }
+
+  deleteHandler(index) {
+    const cars = this.state.cars.concat()
+    cars.splice(index, 1)
+
+    this.setState({cars})
+
+  }
+
+  componentWillMount() {
+    console.log('App componentWillMount')
+  }
+
+  componentDidMount() {
+    console.log('App componentDidMount')
+  }
+
+  render() {
+    console.log('App render')
+    const divStyle = {
+      textAlign: 'center'
+    }
+
+    let cars = null
+
+    if (this.state.showCars) {
+      cars = this.state.cars.map((car, index) => {
+        return (
+          <ErrorBoundary key={index}>
+            <Car
+              name={car.name}
+              year={car.year}
+              index={index}
+              onDelete={this.deleteHandler.bind(this, index)}
+              onChangeName={event => this.onChangeName(event.target.value, index)}
+            />
+          </ErrorBoundary>
+        )
+      })
+    }
+
+    return (
+      <div style={divStyle}>
+        {/*<h1>{this.state.pageTitle}</h1>*/}
+        <h1>{this.props.title}</h1>
+
+        <ClickedContext.Provider value={this.state.clicked}>
+          <Counter />
+        </ClickedContext.Provider>
+
+        <hr/>
+        <button
+          style={{marginTop: 20}}
+          className={'AppButton'}
+          onClick={this.toggleCarsHandler}
+        >Toggle cars</button>
+
+        <button onClick={() => this.setState({clicked: true})}>Change clicked</button>
+
+        <div style={{
+          width: 400,
+          margin: 'auto',
+          paddingTop: '20px'
+        }}>
+          { cars }
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App
---
+++ 2.React JS. Practical Course (2020)/5.Theory.ReactComponents/src/Counter/Counter.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react'
+import Auxiliary from '../hoc/Auxiliary'
+import Counter2 from '../Counter2/Counter2'
+
+export default class Counter extends Component {
+  state = {
+    counter: 0
+  }
+
+  addCounter = () => {
+    this.setState((prevState) => {
+      return {
+        counter: prevState.counter + 1
+      }
+    })
+  }
+
+  render() {
+    return (
+      <Auxiliary>
+        <h2>Counter {this.state.counter}</h2>
+        <Counter2 />
+        <button onClick={this.addCounter}>+</button>
+        <button onClick={() => this.setState({counter: this.state.counter - 1})}>-</button>
+      </Auxiliary>
+    )
+  }
+}
