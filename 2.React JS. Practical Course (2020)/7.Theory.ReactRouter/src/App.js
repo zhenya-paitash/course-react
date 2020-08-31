3198dcbd83181ee35294d0eb6a235aa1729decff
zhenya-paitash <naitavv93@gmail.com>
2020-08-31 19:58:02 +0300

    7.Theory.ReactRouter

---
+++ 2.React JS. Practical Course (2020)/7.Theory.ReactRouter/src/App.js
@@ -0,0 +1,69 @@
+import React, {Component} from 'react'
+import './App.scss'
+import {NavLink, Route, Switch, Redirect} from 'react-router-dom'
+import About from './About/About'
+import Cars from './Cars/Cars'
+import CarDetail from './CarDetail/CarDetail'
+
+class App extends Component {
+
+  state = {
+    isLoggedIn: false
+  }
+
+  render() {
+
+    return (
+      <div>
+        <nav className="nav">
+          <ul>
+            <li>
+              <NavLink
+                to="/"
+                exact
+                activeClassName={'wfm-active'}
+              >Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about" activeStyle={{
+                color: 'blue'
+              }}>About</NavLink>
+            </li>
+            <li>
+              <NavLink to={{
+                pathname: '/cars/'
+              }}>Cars</NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        <hr/>
+        <div style={{textAlign: 'center'}}>
+          <h3>Is logged in {this.state.isLoggedIn ? 'TRUE' : 'FALSE'}</h3>
+          <button onClick={() => this.setState({isLoggedIn: true})}>Login</button>
+        </div>
+
+
+        <hr/>
+
+        {/*localhost:3000*/}
+        <Switch>
+          <Route path="/" exact render={() => <h1>Home Page</h1>}/>
+
+
+          { this.state.isLoggedIn ? <Route path="/about" component={About}/> : null }
+
+
+          <Route path="/cars/:name" component={CarDetail}/>
+          <Route path="/cars" component={Cars}/>
+          <Redirect to={'/'}/>
+          {/*<Route render={() => <h1 style={{color: 'red', textAlign: 'center'}}>404 not found</h1>} />*/}
+        </Switch>
+
+
+      </div>
+    );
+  }
+}
+
+export default App
