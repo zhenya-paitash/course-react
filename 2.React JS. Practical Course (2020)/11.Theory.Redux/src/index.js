65d219da2c99c231428f36d269f85e39bae10142
zhenya-paitash <naitavv93@gmail.com>
2020-09-06 15:40:01 +0300

    11.Theory.Redux

--- 2.React JS. Practical Course (2020)/11.Theory.Redux/src/index.js
+++ 2.React JS. Practical Course (2020)/11.Theory.Redux/src/index.js
@@ -3,7 +3,44 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {createStore, applyMiddleware, compose} from 'redux'
+import {Provider} from 'react-redux'
+import reduxThunk from 'redux-thunk'
+import rootReducer from './redux/rootReducer'
 
+// function loggerMiddleware(store) {
+//   return function(next) {
+//     return function(action) {
+//       const result = next(action)
+//       console.log('Middleware', store.getState())
+//       return result
+//     }
+//   }
+// }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+const loggerMiddleware = store => next => action => {
+  const result = next(action)
+  console.log('Middleware', store.getState())
+  return result
+}
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(
+  loggerMiddleware,
+  reduxThunk
+)))
+
+const app = (
+  <Provider store={store}>
+    <App/>
+  </Provider>
+)
+
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
