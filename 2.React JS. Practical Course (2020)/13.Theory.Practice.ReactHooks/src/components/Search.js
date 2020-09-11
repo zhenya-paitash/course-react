4aa4aeff7ec49d9cadb5217a7d6d25b09dcbe97a
zhenya-paitash <naitavv93@gmail.com>
2020-09-11 15:46:19 +0300

    13.Theory.Practice.ReactHooks#alertContext

--- 2.React JS. Practical Course (2020)/13.Theory.Practice.ReactHooks/src/components/Search.js
+++ 2.React JS. Practical Course (2020)/13.Theory.Practice.ReactHooks/src/components/Search.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {AlertContext} from "../context/alert/alertContext";
 
 
 export const Search = () => {
+  const {show} = useContext(AlertContext);
+  const onSubmit = e => {
+    if (e.key === 'Enter') show('Enter alert!')
+  };
+
   return (
     <div className="form-group">
       <input
         className="form-control"
         placeholder="Enter username..."
+        onKeyPress={onSubmit}
       />
     </div>
   );
