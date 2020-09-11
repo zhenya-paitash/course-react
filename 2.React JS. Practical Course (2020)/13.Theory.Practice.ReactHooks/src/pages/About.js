a1933b686cc7cdd7cd9c3e85cc07b9105cd62f60
zhenya-paitash <naitavv93@gmail.com>
2020-09-11 14:48:15 +0300

    13.Theory.Practice.ReactHooks#routing

---
+++ 2.React JS. Practical Course (2020)/13.Theory.Practice.ReactHooks/src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+export const About = props => {
+  return (
+    <div className="jumbotron jumbotron-fluid">
+      <div className="container">
+        <h1 className="display-4">About</h1>
+        <p className="lead">
+          Application version: <strong>1.0.0</strong><br/>
+          Date start: <strong>11.09.20</strong><br/>
+          Github repository: <a href="https://github.com/zhenya-paitash/course_react/tree/master/2.React%20JS.%20Practical%20Course%20(2020)/13.Theory.Practice.ReactHooks">link</a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
---
+++ 2.React JS. Practical Course (2020)/13.Theory.Practice.ReactHooks/src/pages/Profile.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Profile = props => {
+  return (
+    <div>
+      <h1>Profile page</h1>
+    </div>
+  );
+};
