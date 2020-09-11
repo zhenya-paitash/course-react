bb895f1e37b4aea1adeeb48a2bd37792e361401d
zhenya-paitash <naitavv93@gmail.com>
2020-09-11 17:17:41 +0300

    13.Theory.Practice.ReactHooks#githubRequests

--- 2.React JS. Practical Course (2020)/13.Theory.Practice.ReactHooks/src/components/Search.js
+++ 2.React JS. Practical Course (2020)/13.Theory.Practice.ReactHooks/src/components/Search.js
@@ -1,15 +1,17 @@
 import React, {useContext, useState} from 'react';
 import {AlertContext} from "../context/alert/alertContext";
+import {GithubContext} from "../context/github/githubContext";
 
 
 export const Search = () => {
   const [value, setValue] = useState('');
   const {show} = useContext(AlertContext);
+  const github = useContext(GithubContext);
   const onSubmit = e => {
     if (e.key !== 'Enter') return;
 
     if (value.trim()) {
-      console.log('make request with: ', value);
+      github.search(value.trim());
     } else {
       show('Enter username')
     }
--- 2.React JS. Practical Course (2020)/13.Theory.Practice.ReactHooks/src/context/github/GithubState.js
+++ 2.React JS. Practical Course (2020)/13.Theory.Practice.ReactHooks/src/context/github/GithubState.js
@@ -1,7 +1,17 @@
 import React, {useReducer} from "react";
+import axios from "axios";
 import {GithubContext} from "./githubContext";
 import {githubReducer} from "./githubReducer";
 import {CLEAR_USERS, GET_REPOS, GET_USER, SEARCH_USERS, SET_LOADING} from "../types";
+
+
+const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
+// console.log(CLIENT_ID, CLIENT_SECRET);
+
+const withCreds = url => {
+  return `https://api.github.com${url}client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+};
 
 
 export const GithubState = ({children}) => {
@@ -17,36 +27,36 @@ export const GithubState = ({children}) => {
   const search = async value => {
     setLoading();
 
-    // ...
+    const response = await axios.get(withCreds(`/search/users?q=${value}&`));
 
     dispatch({
       type: SEARCH_USERS,
-      payload: []
+      payload: response.data.items
     });
   };
   const getUser = async name => {
     setLoading();
 
-    // ...
+    const response = await axios.get(withCreds(`/users/${name}?`));
 
     dispatch({
       type: GET_USER,
-      payload: []
+      payload: response.data
     })
   };
   const getRepos = async name => {
     setLoading();
 
-    // ...
+    const response = await axios.get(withCreds(`/users/${name}/repos?per_page=5&`));
 
     dispatch({
       type: GET_REPOS,
-      payload: []
+      payload: response.data
     })
   };
   const clearUsers = () => dispatch({type: CLEAR_USERS});
   const setLoading = () => dispatch({type: SET_LOADING});
-  
+
   const {user, users, repos, loading} = state;
 
 
