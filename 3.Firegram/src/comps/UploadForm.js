5106d373b0a95b2c3a18f87137aff44a7a63ebd2
zhenya-paitash <naitavv93@gmail.com>
2020-09-13 15:13:58 +0300

    3.Firegram#fbStorageHook

--- 3.Firegram/src/comps/UploadForm.js
+++ 3.Firegram/src/comps/UploadForm.js
@@ -26,7 +26,11 @@ const UploadForm = () => {
 
   return (
     <form>
-      <input type="file" onChange={changeHandler} />
+      <label>
+        <input type="file" onChange={changeHandler} />
+        <span>+</span>
+      </label>
+
       <div className="output">
         { error && <div className="error">{ error }</div> }
         { file && <div>{ file.name }</div> }
