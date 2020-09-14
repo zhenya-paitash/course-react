c6753c2b54a6d270d737db75720e408d5a2fdca1
zhenya-paitash <naitavv93@gmail.com>
2020-09-14 16:15:45 +0300

    15.TestingReact

---
+++ 2.React JS. Practical Course (2020)/15.TestingReact/src/App.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import App from './App';
+import Character from './Character';
+
+
+configure({
+  adapter: new Adapter()
+});
+
+describe('<App />', () => {
+
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<App />)
+  );
+
+  it('Should render 3 characters in light side', () => {
+    expect(wrapper.find(Character)).toHaveLength(3);
+  });
+
+  it('Should render 2 characters in dark side', () => {
+    wrapper.setProps({side: 'dark'});
+    expect(wrapper.find(Character)).toHaveLength(2);
+  });
+
+
+})
