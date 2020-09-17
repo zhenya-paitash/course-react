02ae0cdcd7495233dd002fc2c95dbbd463ce7bcc
zhenya-paitash <naitavv93@gmail.com>
2020-09-17 14:01:32 +0300

    03 user model & req body validation

--- 4.MERN Stack Front To Back Full Stack React, Redux & Node.js (2019)/API/server.js
+++ 4.MERN Stack Front To Back Full Stack React, Redux & Node.js (2019)/API/server.js
@@ -7,6 +7,9 @@ const PORT = process.env.PORT || 5000;
 // Connect Database
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => {
   res.send('API Running');
 });
