14997322e9b6b9c6da9b61c797584bac1612bc3c
zhenya-paitash <naitavv93@gmail.com>
2020-09-18 17:00:50 +0300

    05 like/unlike posts

--- 4.MERN Stack Front To Back Full Stack React, Redux & Node.js (2019)/API/routes/api/posts.js
+++ 4.MERN Stack Front To Back Full Stack React, Redux & Node.js (2019)/API/routes/api/posts.js
@@ -104,4 +104,59 @@ router.delete('/:id', auth, async (req, res) => {
 });
 
 
+// @route   PUT api/posts/like/:id
+// @desc    Like a post
+// @access  Private
+router.put('/like/:id', auth, async (req, res) => {
+  try {
+
+    const post = await Post.findById(req.params.id);
+
+    // Check if the post has alredy been liked
+    if (post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+      return res.status(400).json({ msg: 'Post alredy liked' });
+    } 
+
+    post.likes.unshift({ user: req.user.id });
+    await post.save();
+    res.json(post.likes);
+
+  } catch(e) {
+    console.error(e.message);
+    // if (e.kind === 'ObjectId') 
+    //   return res.status(404).json({ msg: 'Post not found' });
+    res.status(500).send('Server Error');
+  }
+});
+
+
+// @route   PUT api/posts/unlike/:id
+// @desc    Unlike a post
+// @access  Private
+router.put('/unlike/:id', auth, async (req, res) => {
+  try {
+
+    const post = await Post.findById(req.params.id);
+
+    // Check if the post has alredy been liked
+    if (post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+      return res.status(400).json({ msg: 'Post has not yet been liked' });
+    } 
+
+    // Get remove index
+    const removeIdx = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
+    post.likes.splice(removeIdx, 1);
+    
+    await post.save();
+    res.json(post.likes);
+
+  } catch(e) {
+    console.error(e.message);
+    // if (e.kind === 'ObjectId') 
+    //   return res.status(404).json({ msg: 'Post not found' });
+    res.status(500).send('Server Error');
+  }
+});
+
+
 module.exports = router;
