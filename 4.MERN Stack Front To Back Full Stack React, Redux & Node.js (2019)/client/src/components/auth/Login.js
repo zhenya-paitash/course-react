95b90a3b45e996827b008372e6bd319fb94650a9
zhenya-paitash <naitavv93@gmail.com>
2020-09-19 19:56:24 +0300

    06 sign up/login pages

--- 4.MERN Stack Front To Back Full Stack React, Redux & Node.js (2019)/client/src/components/auth/Login.js
+++ 4.MERN Stack Front To Back Full Stack React, Redux & Node.js (2019)/client/src/components/auth/Login.js
@@ -1,10 +1,53 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
+import { Link } from 'react-router-dom'
+
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+
+  const { email, password } = formData;
+
+  const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+  const onSubmit = async e => {
+    e.preventDefault();
+    console.log('SUCCESS');
+  };
+
   return (
-    <div>
-      Login
-    </div>
+    <Fragment>
+      <h1 className="large text-primary">Login</h1>
+      <p className="lead"><i className="fas fa-user"></i> Sign Into Your Account</p>
+      <form className="form" onSubmit={e => onSubmit(e)}>
+        <div className="form-group">
+          <input 
+            type="email" 
+            value={email} 
+            onChange={e => onChange(e)} 
+            placeholder="Email Address" 
+            name="email" 
+            required 
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            value={password} 
+            onChange={e => onChange(e)}
+            placeholder="Password"
+            name="password"
+            minLength="6"
+            required
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign Up</Link>
+      </p>
+    </Fragment>
   )
 }
 
--- 4.MERN Stack Front To Back Full Stack React, Redux & Node.js (2019)/client/src/components/auth/Register.js
+++ 4.MERN Stack Front To Back Full Stack React, Redux & Node.js (2019)/client/src/components/auth/Register.js
@@ -1,4 +1,6 @@
 import React, { Fragment, useState } from 'react'
+import { Link } from 'react-router-dom'
+// import axios from 'axios'
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -11,12 +13,29 @@ const Register = () => {
   const { name, email, password, password2 } = formData;
 
   const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
-  const onSubmit = e => {
+  const onSubmit = async e => {
     e.preventDefault();
     if (password !== password2) {
       console.log('Passwords do not mutch');
     } else {
-      console.log(formData);
+      // const newUser = { name, email, password };
+
+      // try {
+
+      //   const config = {
+      //     headers: {
+      //       'Content-Type': 'application/json'
+      //     }
+      //   };
+      //   const body = JSON.stringify(newUser);
+      //   const res = await axios.post(`/api/users`, body, config);
+      //   console.log(res.data);
+
+      // } catch(e) {
+      //   console.error(e.response.data);
+      // }
+
+      console.log('SUCCESS');
     }
   };
 
@@ -73,7 +92,7 @@ const Register = () => {
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to="/login">Login</Link>
       </p>
     </Fragment>
   )
