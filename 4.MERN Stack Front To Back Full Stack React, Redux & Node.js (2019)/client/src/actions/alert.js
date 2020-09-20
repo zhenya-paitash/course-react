ce8fe41ed317ad2463263beca5f797dd0c7fd18f
zhenya-paitash <naitavv93@gmail.com>
2020-09-20 18:05:30 +0300

    07 alert component & actions calls

--- 4.MERN Stack Front To Back Full Stack React, Redux & Node.js (2019)/client/src/actions/alert.js
+++ 4.MERN Stack Front To Back Full Stack React, Redux & Node.js (2019)/client/src/actions/alert.js
@@ -1,11 +1,13 @@
-import uuid from 'uuid';
-import { SET_ALERT } from './types';
+import { v4 as uuidv4 } from 'uuid';
+import { REMOVE_ALERT, SET_ALERT } from './types';
 
-export const setAlert = (msg, alertType) => dispatch => {
-  const id = uuid.v4();
+export const setAlert = (msg, alertType, timeout = 5000) => dispatch => {
+  const id = uuidv4();
   
   dispatch({
     type: SET_ALERT,
     payload: { msg, alertType, id }
   });
+
+  setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
 };
