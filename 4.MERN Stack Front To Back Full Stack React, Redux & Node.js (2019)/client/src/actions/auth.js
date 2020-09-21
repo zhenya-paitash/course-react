195fe8db0510a2ce9c348f02db03a4ed65719f99
zhenya-paitash <naitavv93@gmail.com>
2020-09-21 15:11:25 +0300

    08 load user & set auth token

--- 4.MERN Stack Front To Back Full Stack React, Redux & Node.js (2019)/client/src/actions/auth.js
+++ 4.MERN Stack Front To Back Full Stack React, Redux & Node.js (2019)/client/src/actions/auth.js
@@ -1,38 +1,60 @@
-import axios from 'axios'
-import { setAlert } from './alert'
-import { REGISTER_SUCCESS, REGISTER_FAIL } from './types'
+import axios from 'axios';
+import { setAlert } from './alert';
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+} from './types';
+import setAuthToken from '../utils/setAuthToken';
 
+// Load User
+export const loadUser = () => async (dispatch) => {
+  if (localStorage.token) {
+    setAuthToken(localStorage.token);
+  }
+
+  try {
+    const res = await axios.get('/api/auth');
+    dispatch({
+      type: USER_LOADED,
+      payload: res.data,
+    });
+  } catch (e) {
+    dispatch({
+      type: AUTH_ERROR,
+    });
+  }
+};
 
 // Register User
-export const register = ({ name, email, password }) => async dispatch => {
+export const register = ({ name, email, password }) => async (dispatch) => {
   const config = {
-    baseURL: 'http://localhost:5000',
+    // baseURL: 'http://localhost:5000',
     heders: {
       // 'user-agent': 'node.js',
       'Content-Type': 'application/json',
       // 'Access-Control-Allow-Origin': '*'
-    }
-  }
+    },
+  };
 
   // const body = JSON.stringify({ name, email, password });
   const body = { name, email, password };
 
   try {
-
     const res = await axios.post('/api/users', body, config);
     dispatch({
       type: REGISTER_SUCCESS,
-      payload: res.data
+      payload: res.data,
     });
-
-  } catch(e) {
+  } catch (e) {
     const errors = e.response.data.errors;
     if (errors) {
-      errors.forEach(err => dispatch(setAlert(err.msg, 'danger')))
+      errors.forEach((err) => dispatch(setAlert(err.msg, 'danger')));
     }
 
     dispatch({
-      type: REGISTER_FAIL
+      type: REGISTER_FAIL,
     });
   }
 };
--- 4.MERN Stack Front To Back Full Stack React, Redux & Node.js (2019)/client/src/reducers/auth.js
+++ 4.MERN Stack Front To Back Full Stack React, Redux & Node.js (2019)/client/src/reducers/auth.js
@@ -1,31 +1,44 @@
-import { REGISTER_FAIL, REGISTER_SUCCESS } from "../actions/types";
+import {
+  REGISTER_FAIL,
+  REGISTER_SUCCESS,
+  USER_LOADED,
+  AUTH_ERROR,
+} from '../actions/types';
 
 const initialState = {
   token: localStorage.getItem('token'),
   isAuthenticated: null,
   loading: true,
-  user: null
+  user: null,
 };
 
-export default function(state = initialState, action) {
+export default function (state = initialState, action) {
   const { type, payload } = action;
 
-  switch(type) {
+  switch (type) {
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: payload,
+      };
     case REGISTER_SUCCESS:
       localStorage.setItem('token', payload.token);
-      return { 
-        ...state, 
+      return {
+        ...state,
         ...payload,
         isAuthenticated: true,
-        loading: false
+        loading: false,
       };
     case REGISTER_FAIL:
+    case AUTH_ERROR:
       localStorage.removeItem('token');
-      return { 
-        ...state, 
+      return {
+        ...state,
         token: null,
         isAuthenticated: false,
-        loading: false
+        loading: false,
       };
     default:
       return state;
