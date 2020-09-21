bc37c7c516ebb09c015543e5d65ce8cde0ade93e
zhenya-paitash <naitavv93@gmail.com>
2020-09-21 16:03:18 +0300

    08 user login

--- 4.MERN Stack Front To Back Full Stack React, Redux & Node.js (2019)/client/src/actions/auth.js
+++ 4.MERN Stack Front To Back Full Stack React, Redux & Node.js (2019)/client/src/actions/auth.js
@@ -5,6 +5,8 @@ import {
   REGISTER_FAIL,
   USER_LOADED,
   AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
 } from './types';
 import setAuthToken from '../utils/setAuthToken';
 
@@ -15,11 +17,13 @@ export const loadUser = () => async (dispatch) => {
   }
 
   try {
+
     const res = await axios.get('/api/auth');
     dispatch({
       type: USER_LOADED,
       payload: res.data,
     });
+    
   } catch (e) {
     dispatch({
       type: AUTH_ERROR,
@@ -42,11 +46,14 @@ export const register = ({ name, email, password }) => async (dispatch) => {
   const body = { name, email, password };
 
   try {
+
     const res = await axios.post('/api/users', body, config);
     dispatch({
       type: REGISTER_SUCCESS,
       payload: res.data,
     });
+    dispatch(loadUser());
+
   } catch (e) {
     const errors = e.response.data.errors;
     if (errors) {
@@ -55,6 +62,41 @@ export const register = ({ name, email, password }) => async (dispatch) => {
 
     dispatch({
       type: REGISTER_FAIL,
+    });
+  }
+};
+
+// Login User
+export const login = (email, password) => async (dispatch) => {
+  const config = {
+    // baseURL: 'http://localhost:5000',
+    heders: {
+      // 'user-agent': 'node.js',
+      'Content-Type': 'application/json',
+      // 'Access-Control-Allow-Origin': '*'
+    },
+  };
+
+  // const body = JSON.stringify({ email, password });
+  const body = { email, password };
+
+  try {
+
+    const res = await axios.post('/api/auth', body, config);
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: res.data,
+    });
+    dispatch(loadUser());
+
+  } catch (e) {
+    const errors = e.response.data.errors;
+    if (errors) {
+      errors.forEach((err) => dispatch(setAlert(err.msg, 'danger')));
+    }
+
+    dispatch({
+      type: LOGIN_FAIL,
     });
   }
 };
