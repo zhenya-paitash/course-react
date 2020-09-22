ef45572d5cb94ee419d3f5c9a25505044037c04c
zhenya-paitash <naitavv93@gmail.com>
2020-09-22 18:03:34 +0300

    09 add experience & education

--- 4.MERN Stack Front To Back Full Stack React, Redux & Node.js (2019)/client/src/App.js
+++ 4.MERN Stack Front To Back Full Stack React, Redux & Node.js (2019)/client/src/App.js
@@ -9,6 +9,8 @@ import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PribateRoute';
 import CreateProfile from './components/profile-form/CreateProfile';
 import EditProfile from './components/profile-form/EditProfile';
+import AddExperience from './components/profile-form/AddExperience';
+import AddEducation from './components/profile-form/AddEducation';
 import './App.css';
 // Redux
 import { Provider } from 'react-redux';
@@ -39,6 +41,8 @@ const App = () => {
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
               <PrivateRoute exact path='/create-profile' component={CreateProfile} />
               <PrivateRoute exact path='/edit-profile' component={EditProfile} />
+              <PrivateRoute exact path='/add-experience' component={AddExperience} />
+              <PrivateRoute exact path='/add-education' component={AddEducation} />
             </Switch>
           </section>
         </Fragment>
--- 4.MERN Stack Front To Back Full Stack React, Redux & Node.js (2019)/client/src/reducers/profile.js
+++ 4.MERN Stack Front To Back Full Stack React, Redux & Node.js (2019)/client/src/reducers/profile.js
@@ -1,4 +1,9 @@
-import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE } from '../actions/types';
+import {
+  GET_PROFILE,
+  PROFILE_ERROR,
+  CLEAR_PROFILE,
+  UPDATE_PROFILE,
+} from '../actions/types';
 
 const initialState = {
   profile: null,
@@ -13,6 +18,7 @@ export default function (state = initialState, action) {
 
   switch (type) {
     case GET_PROFILE:
+    case UPDATE_PROFILE:
       return {
         ...state,
         profile: payload,
@@ -29,8 +35,8 @@ export default function (state = initialState, action) {
         ...state,
         profile: null,
         repos: [],
-        loading: false
-      }
+        loading: false,
+      };
     default:
       return state;
   }
