d8c81b4f3dfd064af4a20bbeb8180b378ef4cccd
zhenya-paitash <naitavv93@gmail.com>
2020-09-23 19:22:38 +0300

    10 profile experience & profile education

--- 4.MERN Stack Front To Back Full Stack React, Redux & Node.js (2019)/client/src/components/profile/Profile.js
+++ 4.MERN Stack Front To Back Full Stack React, Redux & Node.js (2019)/client/src/components/profile/Profile.js
@@ -4,6 +4,8 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience';
+import ProfileEducation from './ProfileEducation';
 import { getProfileById } from '../../actions/profile';
 import { Link } from 'react-router-dom';
 
@@ -29,12 +31,41 @@ const Profile = ({
           {auth.isAuthenticated &&
             auth.loading === false &&
             auth.user._id === profile.user._id && (
-              <Link to='/edit-profile' className='btn btn-dark'>Edit Profile</Link>
+              <Link to='/edit-profile' className='btn btn-dark'>
+                Edit Profile
+              </Link>
             )}
-            <div className="profile-grid my-1">
-              <ProfileTop profile={profile} />
-              <ProfileAbout profile={profile} />
+
+          <div className='profile-grid my-1'>
+            <ProfileTop profile={profile} />
+            <ProfileAbout profile={profile} />
+            <div className='profile-exp bg-white p-2'>
+              <h2 className='text-primary'>Experience</h2>
+              {profile.experience.length > 0 ? (
+                <Fragment>
+                  {profile.experience.map((exp) => (
+                    <ProfileExperience key={exp._id} experience={exp} />
+                  ))}
+                </Fragment>
+              ) : (
+                <h4>No experience credentials</h4>
+              )}
             </div>
+
+            <div className='profile-edu bg-white p-2'>
+              <h2 className='text-primary'>Education</h2>
+              {profile.education.length > 0 ? (
+                <Fragment>
+                  {profile.education.map((edu) => (
+                    <ProfileEducation key={edu._id} education={edu} />
+                  ))}
+                </Fragment>
+              ) : (
+                <h4>No education credentials</h4>
+              )}
+            </div>
+
+          </div>
         </Fragment>
       )}
     </Fragment>
