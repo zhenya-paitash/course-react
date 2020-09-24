f3490cdba62f780cab844ee97a0f1855e274409f
zhenya-paitash <naitavv93@gmail.com>
2020-09-25 01:54:49 +0300

    deploy (before 'api' -> '../')

--- 4.MERN Stack Front To Back Full Stack React, Redux & Node.js (2019)/API/server.js
+++ 4.MERN Stack Front To Back Full Stack React, Redux & Node.js (2019)/API/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const path = require('path');
 
 
 // Connect Database
@@ -10,9 +10,6 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.send('API Running');
-});
 // Header config middleware
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -29,5 +26,17 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('../client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
+  });
+};
+
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
