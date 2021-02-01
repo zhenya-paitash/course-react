82adea5f99dbc934bdcda3adb14787b780aaea11
zhenya-paitash <naitavv93@gmail.com>
2021-02-01 14:59:55 +0300

    02. Starting The Front End - react router

--- 5.MERN eCommerce From Scratch (2020)/frontend/src/components/Header.js
+++ 5.MERN eCommerce From Scratch (2020)/frontend/src/components/Header.js
@@ -1,17 +1,28 @@
 import React from 'react'
+import { LinkContainer } from 'react-router-bootstrap'
 import { Navbar, Nav, Container } from 'react-bootstrap'
 
 const Header = () => {
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
-        <Navbar.Brand href='/'>React-Bootstrap</Navbar.Brand>
         <Container>
+          <LinkContainer to='/'>
+            <Navbar.Brand>proShop</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ml-auto'>
-              <Nav.Link href='/cart'><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
-              <Nav.Link href='/login'><i className='fas fa-user'></i> Sign Up</Nav.Link>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <i className='fas fa-shopping-cart'></i> Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/login'>
+                <Nav.Link>
+                  <i className='fas fa-user'></i> Sign Up
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
--- 5.MERN eCommerce From Scratch (2020)/frontend/src/components/Product.js
+++ 5.MERN eCommerce From Scratch (2020)/frontend/src/components/Product.js
@@ -1,21 +1,22 @@
 import React from 'react'
 import Rating from './Rating'
+import { Link } from 'react-router-dom'
 import { Card } from 'react-bootstrap'
 
 const Product = ({ product }) => {
   const { _id: id, image, name, rating, numReviews, price } = product
   return (
     <Card className='my-3 p-3 rounded'>
-      <a href={`/product/${id}`}>
+      <Link to={`/product/${id}`}>
         <Card.Img src={image} variant='top' />
-      </a>
+      </Link>
 
       <Card.Body>
-        <a href={`/product/${id}`}>
+        <Link to={`/product/${id}`}>
           <Card.Title as='div'>
             <strong>{name}</strong>
           </Card.Title>
-        </a>
+        </Link>
 
         <Card.Text as='div'>
           <Rating value={rating} text={`${numReviews} reviews`} />
