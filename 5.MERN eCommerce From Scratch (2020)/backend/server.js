485ee0ac04ed323dbd0870cf69ff37e320ba8e21
zhenya-paitash <naitavv93@gmail.com>
2021-02-05 14:44:52 +0300

    03. Serving & Fetching Data From Express - fetching products from react (useState, useEffect)

--- 5.MERN eCommerce From Scratch (2020)/backend/server.js
+++ 5.MERN eCommerce From Scratch (2020)/backend/server.js
@@ -4,15 +4,15 @@ const products = require('./data/products')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('API is running...')
+  res.status(200).send('API is running...')
 })
 
 app.get('/api/products', (req, res) => {
-  res.json(products)
+  res.status(200).json(products)
 })
 
 app.get('/api/products/:id', (req, res) => {
-  res.json(products.find((p) => p._id === req.params.id))
+  res.status(200).json(products.find((p) => p._id === req.params.id))
 })
 
 app.listen(5000, console.log('Server running on port 5000'))
