fa8a26c41bd0092f1f7699eacc883b791be486fb
zhenya-paitash <naitavv93@gmail.com>
2021-02-08 17:07:19 +0300

    04. Getting Started With MongoDB - fetching products from the database

--- 5.MERN eCommerce From Scratch (2020)/backend/server.js
+++ 5.MERN eCommerce From Scratch (2020)/backend/server.js
@@ -2,28 +2,24 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
-import products from './data/products.js'
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
-
 connectDB()
 
 const app = express()
+// app.use(express.json())
 
 app.get('/', (req, res) => {
   res.status(200).send('API is running...')
 })
 
-app.get('/api/products', (req, res) => {
-  res.status(200).json(products)
-})
-
-app.get('/api/products/:id', (req, res) => {
-  res.status(200).json(products.find((p) => p._id === req.params.id))
-})
+app.use('/api/products', productRoutes)
 
 const PORT = process.env.PORT || 5000
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold
+  )
 )
