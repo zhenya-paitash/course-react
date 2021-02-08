48182c0c6cad612c307127c68fe2138166d4cdde
zhenya-paitash <naitavv93@gmail.com>
2021-02-08 14:21:00 +0300

    04. Getting Started With MongoDB - add colors to console

--- 5.MERN eCommerce From Scratch (2020)/backend/config/db.js
+++ 5.MERN eCommerce From Scratch (2020)/backend/config/db.js
@@ -8,9 +8,9 @@ const connectDB = async () => {
       useCreateIndex: true,
     })
 
-    console.log(`MongoDB connected: ${conn.connection.host}`)
+    console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline)
   } catch (error) {
-    console.error(`MongoDB Error: ${error.message}`)
+    console.error(`MongoDB Error: ${error.message}`.red.underline.bold)
     process.exit(1)
   }
 }
