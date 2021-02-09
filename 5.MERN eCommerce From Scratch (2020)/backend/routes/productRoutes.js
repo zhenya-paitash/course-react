262e5a4b119a70ce4c356aa65cf67af3d588142d
zhenya-paitash <naitavv93@gmail.com>
2021-02-09 12:39:06 +0300

    04. Getting Started With MongoDB - start with postman & custom error handling

--- 5.MERN eCommerce From Scratch (2020)/backend/routes/productRoutes.js
+++ 5.MERN eCommerce From Scratch (2020)/backend/routes/productRoutes.js
@@ -21,19 +21,20 @@ router.get(
 router.get(
   '/:id',
   asyncHandler(async (req, res) => {
-    // const product = await Product.findById(req.params.id)
-    // console.log(product)
+    const product = await Product.findById(req.params.id)
+    console.log(product)
 
-    // if (product) {
-    //   res.status(200).json(product)
-    // } else {
-    //   res.status(404).json({ message: 'Product not found' })
-    // }
-    Product.findById(req.params.id, (err, product) => {
-      err
-        ? res.status(404).json({ message: 'Product not found' })
-        : res.json(product)
-    })
+    if (product) {
+      res.status(200).json(product)
+    } else {
+      res.status(404)
+      throw new Error('Product not found')
+    }
+    // Product.findById(req.params.id, (err, product) => {
+    //   err
+    //     ? res.status(404).json({ message: 'Product not found' })
+    //     : res.json(product)
+    // })
   })
 )
 
--- 5.MERN eCommerce From Scratch (2020)/backend/server.js
+++ 5.MERN eCommerce From Scratch (2020)/backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
@@ -10,11 +11,19 @@ connectDB()
 const app = express()
 // app.use(express.json())
 
+app.use((req, res, next) => {
+  console.log(req.originalUrl)
+  next()
+})
+
 app.get('/', (req, res) => {
   res.status(200).send('API is running...')
 })
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 app.listen(
