881601de856af0b2502b87b22d2b2a30b335083c
zhenya-paitash <naitavv93@gmail.com>
2021-02-17 14:15:02 +0300

    07. Back End User Authentication - clean up by useing Controllers

--- 5.MERN eCommerce From Scratch (2020)/backend/routes/productRoutes.js
+++ 5.MERN eCommerce From Scratch (2020)/backend/routes/productRoutes.js
@@ -1,41 +1,11 @@
 import express from 'express'
-import asyncHandler from 'express-async-handler'
 const router = express.Router()
-import Product from '../models/productModel.js'
+import {
+  getProducts,
+  getProductsById,
+} from '../controllers/productController.js'
 
-// @desc      Fetch all products
-// @route     GET /api/products
-// @access    Public
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({})
-
-    res.status(200).json(products)
-  })
-)
-
-// @desc      Fetch single products
-// @route     GET /api/products/:id
-// @access    Public
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
-    console.log(product)
-
-    if (product) {
-      res.status(200).json(product)
-    } else {
-      res.status(404)
-      throw new Error('Product not found')
-    }
-    // Product.findById(req.params.id, (err, product) => {
-    //   err
-    //     ? res.status(404).json({ message: 'Product not found' })
-    //     : res.json(product)
-    // })
-  })
-)
+router.route('/').get(getProducts)
+router.route('/:id').get(getProductsById)
 
 export default router
