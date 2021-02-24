bf9bea0b038a32810f03f84d2fa51087b7619419
zhenya-paitash <naitavv93@gmail.com>
2021-02-24 18:08:54 +0300

    08. Front End User Authentication & Profile - user register reducer, action and screen

--- 5.MERN eCommerce From Scratch (2020)/frontend/src/actions/userActions.js
+++ 5.MERN eCommerce From Scratch (2020)/frontend/src/actions/userActions.js
@@ -4,13 +4,14 @@ import {
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
   USER_LOGOUT,
+  USER_REGISTER_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
 } from '../constants/userConstants'
 
 export const login = (email, password) => async (dispatch) => {
   try {
-    dispatch({
-      type: USER_LOGIN_REQUEST,
-    })
+    dispatch({ type: USER_LOGIN_REQUEST })
 
     const config = {
       headers: { 'Constent-Type': 'apllication/json' },
@@ -41,5 +42,42 @@ export const login = (email, password) => async (dispatch) => {
 
 export const logout = () => (dispatch) => {
   localStorage.removeItem('userInfo')
+
   dispatch({ type: USER_LOGOUT })
+}
+
+export const register = (name, email, password) => async (dispatch) => {
+  try {
+    dispatch({ type: USER_REGISTER_REQUEST })
+
+    const config = {
+      headers: { 'Constent-Type': 'apllication/json' },
+    }
+
+    const { data } = await axios.post(
+      '/api/users',
+      { name, email, password },
+      config
+    )
+
+    dispatch({
+      type: USER_REGISTER_SUCCESS,
+      payload: data,
+    })
+
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: data,
+    })
+
+    localStorage.setItem('userInfo', JSON.stringify(data))
+  } catch (error) {
+    dispatch({
+      type: USER_REGISTER_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
 }
