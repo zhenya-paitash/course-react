65ebd7ce55dd17e911085c2498d7592ea9d08b6b
zhenya-paitash <naitavv93@gmail.com>
2021-04-10 16:58:06 +0300

    04. Server Side GraphQL Development - Creating our first User data type

--- 6.Advanced React and GraphQL (2021)/backend/keystone.ts
+++ 6.Advanced React and GraphQL (2021)/backend/keystone.ts
@@ -1,4 +1,5 @@
 import { config, createSchema } from '@keystone-next/keystone/schema'
+import { User } from './schemas/User'
 import 'dotenv/config'
 
 const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-sickfits'
@@ -23,6 +24,7 @@ export default config({
   },
   lists: createSchema({
     // ? Schema items go in here
+    User,
   }),
   ui: {
     // ! TODO: change this for roles
