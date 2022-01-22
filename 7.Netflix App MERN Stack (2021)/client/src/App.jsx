8e257c739a7a6ccd6d70e8ccae4d0dbe9962a1e3
zhenya-paitash <naitavv93@gmail.com>
2022-01-22 15:19:09 +0300

    cleared the project for a manual build

--- 7.Netflix App MERN Stack (2021)/client/src/App.jsx
+++ 7.Netflix App MERN Stack (2021)/client/src/App.jsx
@@ -1,11 +1,7 @@
 import "./app.scss"
-import Home from "./pages/home/Home";
-import Register from "./pages/register/Register";
-import Watch from "./pages/watch/Watch";
-import Login from "./pages/login/Login";
 
 const App = () => {
-  return <Home/>;
+  return <h1>Hello World</h1>
 };
 
 export default App;
