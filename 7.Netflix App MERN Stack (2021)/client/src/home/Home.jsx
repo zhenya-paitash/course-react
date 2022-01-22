a2138138fac7813ae9ceb2443b497c91e9c63ed6
zhenya-paitash <naitavv93@gmail.com>
2022-01-22 17:09:04 +0300

    add Featured component

--- 7.Netflix App MERN Stack (2021)/client/src/home/Home.jsx
+++ 7.Netflix App MERN Stack (2021)/client/src/home/Home.jsx
@@ -1,13 +1,14 @@
-import Navbar from '../components/navbar/Navbar';
-import './home.scss'
+import Navbar from "../components/navbar/Navbar"
+import Featured from "../components/featured/Featured"
+import "./home.scss"
 
 const Home = () => {
   return (
     <div className='home'>
       <Navbar />
-      <img width='100%' src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
+      <Featured type='movie' />
     </div>
   )
-};
+}
 
-export default Home;
+export default Home
