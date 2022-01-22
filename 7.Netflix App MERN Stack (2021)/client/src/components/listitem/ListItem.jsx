840f0337c15ef6fffbe15c1ff50a99077739424d
zhenya-paitash <naitavv93@gmail.com>
2022-01-22 18:11:06 +0300

    pure react netflix slider

---
+++ 7.Netflix App MERN Stack (2021)/client/src/components/listitem/ListItem.jsx
@@ -0,0 +1,26 @@
+import {
+  PlayArrow,
+  Add,
+  ThumbUpAltOutlined,
+  ThumbDownOutlined,
+} from "@material-ui/icons"
+import "./listItem.scss"
+
+export default function Listitem() {
+  return (
+    <div className='listItem'>
+      <img
+        src='https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee'
+        alt=''
+      />
+      <div className='itemIfo'>
+        <div className='icons'>
+          <PlayArrow />
+          <Add />
+          <ThumbUpAltOutlined />
+          <ThumbDownOutlined />
+        </div>
+      </div>
+    </div>
+  )
+}
