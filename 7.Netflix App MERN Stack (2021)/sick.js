31254877653519fadb116c2e87ea462b50105a7c
zhenya-paitash <naitavv93@gmail.com>
2022-01-25 20:54:37 +0300

    sick

---
+++ 7.Netflix App MERN Stack (2021)/sick.js
@@ -0,0 +1,7 @@
+function healthStatus(firstname, lastname, status) {
+	return `User: ${[firstname, lastname].join` `} now is ${status}`
+}
+
+
+const myStatus = healthStatus('zhenya', 'paitash', 'SICK')
+console.log(myStatus)
