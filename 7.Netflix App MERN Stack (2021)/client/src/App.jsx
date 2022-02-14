2b544b1b5e9adca76f618ad6a51748715a25c3b0
zhenya-paitash <naitavv93@gmail.com>
2022-02-14 16:48:10 +0300

    register page

--- 7.Netflix App MERN Stack (2021)/client/src/App.jsx
+++ 7.Netflix App MERN Stack (2021)/client/src/App.jsx
@@ -1,10 +1,12 @@
 import "./app.scss"
 import Home from "./pages/home/Home"
 import Watch from "./pages/watch/Watch"
+import Register from "./pages/register/Register"
 
 const App = () => {
   // return <Home />
-  return <Watch />
+  // return <Watch />
+  return <Register />
 }
 
 export default App
