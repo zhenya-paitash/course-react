b6d3d1c7d546079791d269f3c17a547ed83b56a0
zhenya-paitash <naitavv93@gmail.com>
2022-02-14 17:04:29 +0300

    login page. End client presetting

--- 7.Netflix App MERN Stack (2021)/client/src/App.jsx
+++ 7.Netflix App MERN Stack (2021)/client/src/App.jsx
@@ -2,11 +2,13 @@ import "./app.scss"
 import Home from "./pages/home/Home"
 import Watch from "./pages/watch/Watch"
 import Register from "./pages/register/Register"
+import Login from "./pages/login/Login"
 
 const App = () => {
   // return <Home />
   // return <Watch />
-  return <Register />
+  // return <Register />
+  return <Login />
 }
 
 export default App
