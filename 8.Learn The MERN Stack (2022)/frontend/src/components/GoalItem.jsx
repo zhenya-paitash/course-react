56e7d573487d19085e64b0a4300ba97aa6baf168
zhenya-paitash <naitavv93@gmail.com>
2022-04-08 17:30:15 +0300

    Deploy

--- 8.Learn The MERN Stack (2022)/frontend/src/components/GoalItem.jsx
+++ 8.Learn The MERN Stack (2022)/frontend/src/components/GoalItem.jsx
@@ -9,7 +9,7 @@ export default function GoalItem({ goal }) {
       <div>{new Date(goal.createdAt).toLocaleDateString('en-US')}</div>
       <h2>{goal.text}</h2>
       <button onClick={() => dispatch(deleteGoal(goal._id))} className='close'>
-        ❌
+        X
       </button>
     </div>
   )
